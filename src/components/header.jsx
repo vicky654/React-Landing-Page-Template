7b772d0cf15caa3194fa46c585d6a11b67325870
vicky654 <66287987+vicky654@users.noreply.github.com>
2023-05-03 20:27:08 +0530

    register

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export const Header = (props) => {
 
-  const date = new Date();
-  const currentHour = date.getHours();
-  const currentMinute = date.getMinutes();
-  const isHiTime = (currentHour === 3 && currentMinute >= 10) || 
-  (currentHour === 4 || currentHour === 5 && currentMinute >= 0) || 
-  (currentHour === 6 && currentMinute <= 15);
+  // const date = new Date();
+  // const currentHour = date.getHours();
+  // const currentMinute = date.getMinutes();
+  // const isHiTime = (currentHour === 20 && currentMinute >= 26) || 
+  // (currentHour === 20 || currentHour === 20 && currentMinute >= 30) || 
+  // (currentHour === 6 && currentMinute <= 15);
 
   return (
     <header id="header">
@@ -23,22 +23,14 @@ export const Header = (props) => {
                 </h5>
                 {/* <p>{props.data ? props.data.paragraph : "Loading"}</p> */}
               
-
-                {isHiTime ?   <a
-                  href="/register"
+                <a
+                 href="/register"
                   className="btn btn-custom btn-lg page-scroll"
                 >
                   Learn More
                   <i className="fa fa-arrow-down ml-2"></i>
 
-                </a> :  <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Learn More
-                  <i className="fa fa-arrow-down ml-2"></i>
-
-                </a> }
+                </a>
 
               </div>
             </div>
